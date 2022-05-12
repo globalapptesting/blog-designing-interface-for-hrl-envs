0ab49ae9c0c15de2f576731c37f5620b7a9d7e5d
Tomasz Wrona <tomasz@wrona.me>
2022-05-12 14:31:34 +0200

    Add serialization for action interface

--- hrl/action.py
+++ hrl/action.py
@@ -1,22 +1,28 @@
 from abc import ABC
 from dataclasses import dataclass
 
+from dataclasses_json import dataclass_json
 
+
+@dataclass_json
 @dataclass(frozen=True)
 class Action(ABC):
     pass
 
 
+@dataclass_json
 @dataclass(frozen=True)
 class SwitchAgent(Action):
     pass
 
 
+@dataclass_json
 @dataclass(frozen=True)
 class NoSwitchAction(SwitchAgent):
     pass
 
 
+@dataclass_json
 @dataclass(frozen=True)
 class ProcedureRequest(Action):
     pass
